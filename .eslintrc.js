fae71a7077e6d9310b6a20f3e0d72314a1948e89
Artur 'Wodor' Wielogorski <wodor@wodor.net>
2023-12-18 05:55:34 +0000

    excluded automatically generated files from eslint checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:react/recommended'],
   plugins: ['react'],
+  ignorePatterns: ['public/*', 'node_modules/*', 'dist/*', 'build/*', 'coverage/*'],
 };
