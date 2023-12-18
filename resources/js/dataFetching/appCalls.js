264284f78a95d287c4caa43d4dbb4f386eb24fb9
Artur 'Wodor' Wielogorski <wodor@wodor.net>
2023-12-18 09:16:51 +0000

    api calls to obtain turbine inspection data

--- resources/js/dataFetching/appCalls.js
+++ resources/js/dataFetching/appCalls.js
@@ -32,10 +32,50 @@ export async function getWindFarmTurbines(windFarmId) {
 
 export async function getWindFarmInspections(windFarmId) {
   try {
-    const response = await axios.get(`/api/farms/${windFarmId}/inspections`);
+    const response = await axios.get(`/api/inspections/${windFarmId}`);
     const { data } = response.data;
     return { inspections: data, fetchingError: null };
   } catch (error) {
     return { inspections: [], fetchingError: error };
+  }
+}
+
+export async function getTurbineById(turbineId) {
+  try {
+    const response = await axios.get(`/api/turbines/${turbineId}/inspections`);
+    const { data } = response.data;
+    return { turbine: data, fetchingError: null };
+  } catch (error) {
+    return { turbine: [], fetchingError: error };
+  }
+}
+
+export async function getTurbineInspectionsGrade(turbineId) {
+  try {
+    const response = await axios.get(`/api/inspections/${turbineId}/grades`);
+    const { data } = response.data;
+    return { grades: data, fetchingError: null };
+  } catch (error) {
+    return { grades: [], fetchingError: error };
+  }
+}
+
+export async function getTurbineComponents(turbineId) {
+  try {
+    const response = await axios.get(`/api/turbines/${turbineId}/components`);
+    const { data } = response.data;
+    return { components: data, fetchingError: null };
+  } catch (error) {
+    return { components: [], fetchingError: error };
+  }
+}
+
+export async function getComponentTypes() {
+  try {
+    const response = await axios.get('/api/component-types');
+    const { data } = response.data;
+    return { types: data, fetchingError: null };
+  } catch (error) {
+    return { types: [], fetchingError: error };
   }
 }
